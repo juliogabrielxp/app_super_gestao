379cb73bcf7bc79aae1442784a1af6aa66739815
juliogabrielxp <p90degold@gmail.com>
2025-07-29 20:49:24 -0300

    Progressão

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::get('/contato/{nome}/{idade}/{profissao?}', function(string $nome, int $idade, string $profissao = 'profissão não informada') {
     echo "Meu nome é $nome, tenho $idade anos e trabalho como $profissao";
 })->where('nome', '[A-Za-z]+')->where('idade', '[1-99]+');
+
+Route::get('/login', 'LoginController@login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes',function() { return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function() {return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function() {return 'Produtos';} )->name('app.produtos');
+});
+
+
+
+   
+
+
+
+
