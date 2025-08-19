c47ab70a65e6cd2e75082eaf80e4ecebcaaff6d6
juliogabrielxp <p90degold@gmail.com>
2025-08-18 22:17:30 -0300

    Adicionando views

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        $fornecedores = [0 => ['Wurth' => 'Glauco'],
-        1 => ['Tay' => 'Jasmin', 'CPF' => '123']
-    ];
-
-        return view('site.principal',compact('fornecedores'));
+        return view('site.principal');
     }
 }
